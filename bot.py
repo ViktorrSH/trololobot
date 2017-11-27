5a6fb5b7b7cb105cd84eb0e666281ea7bd0007ce
ViktorrSH <32921336+ViktorrSH@users.noreply.github.com>
2017-11-27 15:34:21 +0800

    тест
    залил фул бота

---
+++ bot.py
@@ -0,0 +1,37 @@
+ #-*- coding: utf-8 -*-
+import config
+import datetime
+import telebot
+import random
+from telebot import types
+from telebot import types
+
+bot = telebot.TeleBot(config.token)
+
+hleb = ["https://www.youtube.com/watch?v=OvZUf7YOBmU",
+		"https://www.youtube.com/watch?v=UMfMekip6y8",
+		"https://www.youtube.com/watch?v=1lBsJDludgI"]
+
+
+		
+@bot.message_handler(commands=['start', 'help'])
+
+
+
+#def henn(message):
+#	bot.send_message(message.chat.id, 'ваше видео, повелитель!')
+#	#bot.send_message(message.chat.id, 'https://www.youtube.com/watch?v=OvZUf7YOBmU')
+#	bot.send_message(message.chat.id, random.choice(hleb))
+
+
+#if __name__ == '__main__':
+#     bot.polling(none_stop=True)
+
+def henn(message):
+	i = 1
+	while  i < 1000000:
+		bot.send_message(message.chat.id, 'Глебка, нюхай какашку')
+		pass
+
+if __name__ == '__main__':
+     bot.polling(none_stop=True)
---
+++ config.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+# Этот токен невалидный, можете даже не пробовать :)
+token = '453944472:AAGHgS8VOxDI_pCCM4VXp2EAoUmjUZeB09k'
